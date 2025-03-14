99adde69c2163eaac6cc4b64e7809e38528e324d
Kouakou Jedidiah Ange-Emmanuel <angemmanuel.kouakou+professional@gmail.com>
2025-03-14 19:33:23 +0000

    feat(proximal_step fn): Arguably the most difficult mathematically, implementation I have done to this point.
    It uses a special case of the QP algorithm where the constraint boils down to projecting the solution unto the feasible set.
    
    When I write the improved version with support for more customizable constraints, I'll have to make use of a proper QP solver.

--- src/evolution/portfolio_evolution.rs
+++ src/evolution/portfolio_evolution.rs
@@ -424,9 +424,51 @@ fn lamarckian_proximal_descent(returns: Vec<Vec<f64>>,
     // proximal step returns the new portfolio to use
     proximal_step(&tentative_new_portfolio)
 }
-fn proximal_step()-> Vec<f64> {
-    unimplemented!()
+
+// 
+fn proximal_step(weights: &Vec<f64>)-> Vec<f64> {
+    // In this context, the proximal step reduces to projecting
+    // the weight vector on the simplex defined by w_i >= 0, and sum(w_i) = 1.
+    // Which is equivalent to solving the QP problem 1/2||w-x||^2 subject to the constraints
+
+    // The following code adapts this idea without the overhead of an actual
+    // QP solver
+    let n = weights.len();
+    let mut sorted_weights = weights.clone();
+    sorted_weights.sort_by(|a,b| b.partial_cmp(a).unwrap());
+
+    // Find all the k non-zero values
+    let mut sum = 0.0;
+    let mut k = n;
+
+    for i in 0..n {
+        sum += sorted_weights[i];
+        let theta = (sum-1.0)/(i+1) as f64;
+        if sorted_weights[i] - theta <= 0.0 {
+            k = i; // 1, 2, 3,... kth value, ... nth value
+            break;
+        }
+    }
+
+    // Compute the threshold value
+    let sum_topk = sorted_weights[..k].iter().sum::<f64>();
+    // Distributes the excess across the non-zero values
+    let theta = (sum_topk - 1.0) / k as f64;
+
+    // Finally project
+    let projected_weights = weights.iter().map(|w| (w-theta).max(0.)).collect::<Vec<f64>>();
+
+    // Checks if all weights were squashed to 0.
+    let projected_sum = projected_weights.iter().sum::<f64>();
+    if  projected_sum.abs() < 1e-10 * n as f64 {
+        // return the uniform distribution if weights were already all negative or smth
+        vec![1./n as f64;n]
+    } else {
+        // Final normalization for stability
+        projected_weights.into_iter().map(|w| w/projected_sum).collect::<Vec<f64>>()
+    }
 }
+
 fn compute_portfolio_gradient(returns: Vec<Vec<f64>>,
     weights: Vec<f64>,
     base_performance : PortfolioPerformance,
@@ -461,9 +503,7 @@ objective: Objective,) -> Vec<f64> {
        gradient
     }
 
-fn proximal_step()-> Vec<f64> {
-    unimplemented!()
-}
+
 
 struct PortfolioPerformance {  
     returns: Vec<f64>,
