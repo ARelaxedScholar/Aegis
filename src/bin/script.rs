413f0e05b42c3a7fb193cc592c67bfa6a6e8e05d
ARelaxedScholar <angemmanuel.kouakou+professional@gmail.com>
2025-04-24 11:29:37 +0000

    There were things unstashed apparently

--- src/bin/script.rs
+++ src/bin/script.rs
@@ -23,8 +23,8 @@ async fn main() -> anyhow::Result<()> {
         assets_under_management,
         money_to_invest: 1_000_000.0,
         risk_free_rate: 0.01,
-        elitism_rate: 0.05,
-        mutation_rate: 0.2,
+        elitism_rate: 0.00,
+        mutation_rate: 0.1,
         tournament_size: 3,
         sampler: Sampler::factor_model_synthetic(
             assets_under_management,
